1a53eb07fc868e33dcc3657cc958bdf1ec8280be
DalipTandon <daliptandon8@gmail.com>
2026-08-13 20:32:36 +0530

    abstraction

---
+++ Abstraction/Main.java
@@ -0,0 +1,24 @@
+package Abstraction;
+
+public  class Main extends student {
+            public   void name(){
+                System.out.println("dummy name");
+            };
+
+        public static void main(String[]args){
+        Main m=new Main();
+        m.name();
+        }
+}
+
+
+// public  class Main extends student {
+//             public   void name(){
+//                 System.out.println("dummy name");
+//             };
+
+//         public static void main(String[]args){
+//         Main m=new Main();
+//         m.name();
+//         }
+// }
---
+++ Abstraction/student.java
@@ -0,0 +1,5 @@
+package Abstraction;
+
+public abstract class student {
+    public  abstract void name();
+}
